d50ed23de7c3da7ab5befde453d67eac6324daec
eternitvy <crrissant@gmail.com>
2024-10-10 07:34:52 +0700

    percobaan1

---
+++ SiakadWhile22.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SiakadWhile22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int nilai, jml, i = 0;
+
+        System.out.println("masukkan jumlah mahasiswa: ");
+        jml = sc.nextInt();
+
+        while (i < jml) {
+
+            i++
+        }
+    }
+}
