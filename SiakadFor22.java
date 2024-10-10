54118f78b26d73f7cd082f87f5907137df79ee06
eternitvy <crrissant@gmail.com>
2024-10-10 07:54:45 +0700

    modifikasi1

--- SiakadFor22.java
+++ SiakadFor22.java
@@ -5,6 +5,7 @@ public class SiakadFor22 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int jumlahLulus = 0, jumlahTidakLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("masukkan nilai mahasiswa ke-" + i + ": ");
@@ -15,9 +16,17 @@ public class SiakadFor22 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= 60) {
+                jumlahLulus++;
+            }
+            else {
+                jumlahTidakLulus++;
+            }
         }
 
         System.out.println("nilai tertinggi: " + tertinggi);
         System.out.println("nilai terendah: " + terendah);
+        System.out.println("jumlah mahasiswa lulus: " + jumlahLulus);
+        System.out.println("jumlah mahasiswa tidak lulus: " + jumlahTidakLulus); 
     }
 }
