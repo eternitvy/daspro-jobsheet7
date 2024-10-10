83de43771cea06627c540ba7dee9cf4178807043
eternitvy <crrissant@gmail.com>
2024-10-10 08:05:09 +0700

    percobaan2

--- SiakadWhile22.java
+++ SiakadWhile22.java
@@ -6,12 +6,39 @@ public class SiakadWhile22 {
 
         int nilai, jml, i = 0;
 
-        System.out.println("masukkan jumlah mahasiswa: ");
+        System.out.print("masukkan jumlah mahasiswa: ");
         jml = sc.nextInt();
 
         while (i < jml) {
+            System.out.print("masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilai = sc.nextInt();
 
-            i++
+            if (nilai < 0 || nilai > 100) {
+                System.out.println("nilai tidak valid, masukkan lagi nilai yang valid");
+                continue;
+            }
+            if (nilai > 80 && nilai <= 100) {
+                System.out.println("nilai mahasiswa ke-" + (i + 1) + " adalah A");
+            }
+            else if (nilai > 73 && nilai <= 80) {
+                System.out.println("nilai mahasiswa ke-" + (i + 1) + " adalah B+");
+            }
+            else if (nilai > 65 && nilai <= 73) {
+                System.out.println("nilai mahasiswa ke-" + (i + 1) + " adalah B");
+            }
+            else if (nilai > 60 && nilai <= 65) {
+                System.out.println("nilai mahasiswa ke-" + (i + 1) + " adalah C+");
+            }
+            else if (nilai > 50 && nilai <= 60) {
+                System.out.println("nilai mahasiswa ke-" + (i + 1) + " adalah C");
+            }
+            else if (nilai > 39 && nilai <= 50) {
+                System.out.println("nilai mahasiswa ke-" + (i + 1) + " adalah D");
+            }
+            else {
+                System.out.println("nilai mahasiswa ke-" + (i + 1) + " adalah E");
+            }
+            i++;
         }
     }
 }
